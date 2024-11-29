73d89976b63c29c5eb57742b283164bfb5d32584
PatriciaAlEs <patriciaalvarezestevez@gmail.com>
2024-11-29 12:44:06 +0100

    comiteo de prubea del dia 2

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let numA = 7
+let numB = 5
+
+let suma = numA + numB
+suma ++
+suma ++
+console.log(suma)
+
+console.log("El script.js está funcionando correctamente.");
+alert("Hola desde script.js");
+
